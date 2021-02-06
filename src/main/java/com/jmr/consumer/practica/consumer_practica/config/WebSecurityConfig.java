cd1ed73885f1887c71dd19786ccb4efac16fd629
jmRicardo <51313148+jmRicardo@users.noreply.github.com>
2021-02-06 02:46:48 -0300

    second

---
+++ src/main/java/com/jmr/consumer/practica/consumer_practica/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.jmr.consumer.practica.consumer_practica.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        manager.createUser(User.withUsername("user").password(encoder.encode("secret")).roles("USER").build());
+        return manager;
+    }
+}
