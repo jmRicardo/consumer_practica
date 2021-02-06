6b04fe83974dd2c3dfac234a10d40f9239dce21b
jmRicardo <51313148+jmRicardo@users.noreply.github.com>
2021-02-06 01:58:42 -0300

    first

---
+++ src/main/java/com/jmr/consumer/practica/consumer_practica/ConsumerPracticaApplication.java
@@ -0,0 +1,13 @@
+package com.jmr.consumer.practica.consumer_practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerPracticaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerPracticaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jmr/consumer/practica/consumer_practica/ConsumerPracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmr.consumer.practica.consumer_practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerPracticaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
