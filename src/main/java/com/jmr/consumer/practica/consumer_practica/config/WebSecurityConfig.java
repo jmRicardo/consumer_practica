c5c429b926bdb2964f50da2b75ffee296b4d7006
jmRicardo <51313148+jmRicardo@users.noreply.github.com>
2021-02-09 12:07:34 -0300

    more

--- src/main/java/com/jmr/consumer/practica/consumer_practica/config/WebSecurityConfig.java
+++ src/main/java/com/jmr/consumer/practica/consumer_practica/config/WebSecurityConfig.java
@@ -1,6 +1,9 @@
 package com.jmr.consumer.practica.consumer_practica.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -8,9 +11,28 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
+@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/index", "/css/*", "/js/*").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .and()
+                .logout().invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/logout-success").permitAll();
+    }
 
     @Bean
     @Override
--- src/main/java/com/jmr/consumer/practica/consumer_practica/controller/MainController.java
+++ src/main/java/com/jmr/consumer/practica/consumer_practica/controller/MainController.java
@@ -1,9 +1,12 @@
 package com.jmr.consumer.practica.consumer_practica.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class MainController {
 
     @GetMapping("/test")
@@ -11,6 +14,12 @@ public class MainController {
         {
             return "test";
         }
+
+    @GetMapping(value = {"/", "/index"})
+    public String getHomePage(Model model) {
+
+        return "index";
+    }
 
 
 }
